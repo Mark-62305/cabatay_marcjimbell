c8680a0e51ef660f85d6083578ca56844353ef00
Mark-62305 <cabataymarc31@gmail.com>
2025-09-12 12:50:52 +0800

    good for deployment

--- app/config/database.php
+++ app/config/database.php
@@ -59,11 +59,11 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 $database['main'] = array(
     'driver'	=> 'mysql',
-    'hostname'	=> 'localhost',
+    'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'root',
-    'password'	=> '',
-    'database'	=> 'laval',
+    'username'	=> 'sql12798300',
+    'password'	=> 'wsFctjpVlk',
+    'database'	=> 'sql12798300',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
